8f7775d550404a622340bcbd8e5c61b9ff0faf79
takepro14 <takepro100pm@gmail.com>
2022-08-15 09:13:50 +0900

    Add Refresh Token Class

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,6 +39,16 @@ class User < ApplicationRecord
     users.find_by_activated(email).present?
   end
 
+  # リフレッシュトークンのJWT IDを記憶する
+  def remember(jti)
+    update!(refresh_jti: jti)
+  end
+
+  # リフレッシュトークンのJWT IDを削除する
+  def forget
+    update!(refresh_jti: nil)
+  end
+
   private
 
     def downcase_email
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_11_085500) do
+ActiveRecord::Schema.define(version: 2022_08_15_000332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2022_08_11_085500) do
     t.boolean "admin", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "refresh_jti"
   end
 
   add_foreign_key "posts", "users"
