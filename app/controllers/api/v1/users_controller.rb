7ddd7b397abd180a3a5e86be173c3c7a152de412
takepro14 <takepro100pm@gmail.com>
2022-08-15 10:16:22 +0900

    Add Authenticate Servide Module

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::UsersController < ApplicationController
+  # ログイン済みのユーザしかアクセスさせたくないリソースに設定
+  before_action :authenticate_active_user
+
   def index
-    users = User.all
-    render json: users.as_json(only: [:id, :name, :email, :created_at])
+    # users = User.all
+    # render json: users.as_json(only: [:id, :name, :email, :created_at])
+    render json: current_user.as_json(only: [:id, :name, :email, :created_at])
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  # Cookieを扱う
+  include ActionController::Cookies
+  # 認可を行う
+  include UserAuthenticateService
 end
