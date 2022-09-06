a794f6ce0e31cc9c92194c1055c2333429a5a741
takepro14 <takepro100pm@gmail.com>
2022-09-06 15:30:45 +0900

    [feat] profileカラム追加に伴う修正

--- db/fixtures/development/01_users.rb
+++ db/fixtures/development/01_users.rb
@@ -2,6 +2,7 @@
   User.seed do |s|
     s.name = Faker::Name.name
     s.email = "test-user#{n}@example.com"
+    s.profile = Faker::Lorem.paragraph
     s.password = "password"
     s.activated = true
   end
