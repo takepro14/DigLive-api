b1b7ea918f82e406ad39e4375f1926df8ef65ec0
takepro14 <takepro100pm@gmail.com>
2022-09-14 15:17:16 +0900

    [feat] 通知を画面表示できるようにする

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -6,12 +6,13 @@ class Api::V1::NotificationsController < ApplicationController
   def index
     # binding.pry
     @user = User.find(current_user.id)
-    @notifications = @user.passive_notifications.includes(:visitor, :visited, :post)
+    @notifications = @user.passive_notifications.includes(:visitor, :visited, :post, :comment)
 
-    render json: @notifications.as_json(include:
-                                          [:visitor,
+    render json: @notifications.as_json(include:[
+                                            :visitor,
                                             :visited,
-                                            :post
+                                            :post,
+                                            :comment
                                           ]
                                         )
   end
--- db/fixtures/development/11_notificatios.rb
+++ db/fixtures/development/11_notificatios.rb
@@ -1,23 +1,23 @@
 10.times do |t|
+  # フォロー
+  Notification.seed do |s|
+    s.action = 'follow'
+    s.visitor_id = t + 1
+    s.visited_id = t + 2
+  end
   # いいね
   Notification.seed do |s|
+    s.action = 'like'
     s.visitor_id = t + 1
     s.visited_id = t + 2
     s.post_id = t + 1
-    s.action = 'like'
   end
   # コメント
   Notification.seed do |s|
-    s.visitor_id = t + 1
-    s.visited_id = t + 2
-    s.comment_id = t + 1
     s.action = 'comment'
-  end
-  # フォロー
-  Notification.seed do |s|
     s.visitor_id = t + 1
     s.visited_id = t + 2
     s.post_id = t + 1
-    s.action = 'follow'
+    s.comment_id = t + 1
   end
 end
