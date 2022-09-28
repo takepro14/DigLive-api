8650b2e05e665e6b5ef29899c21bf40fa8b9c55f
takepro14 <takepro100pm@gmail.com>
2022-09-28 18:36:33 +0900

    [refactor] 通知機能のvuex化

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -6,6 +6,7 @@ class Api::V1::NotificationsController < ApplicationController
   def index
     @user = User.find(current_user.id)
     @notifications = @user.passive_notifications.includes(:visitor, :visited, :post, :comment)
+    @notifications.where(checked: false).each{|notification| notification.update_attributes(checked: true)}
 
     render json: @notifications.as_json(include:[
                                             :visitor,
