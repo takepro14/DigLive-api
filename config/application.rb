b0384b1aa2aec7ebe88511c04ca4c1bc5250d38b
takepro14 <takepro100pm@gmail.com>
2022-10-01 21:10:16 +0900

    [style] faviconの追加

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module App
+module DigLive
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     # 6.0 => Zeitwerk(オートロードシステム)を有効にする
