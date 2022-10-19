fb8d53889b9c2457286fd72e744eb9f5aa46cf70
takepro14 <takepro100pm@gmail.com>
2022-10-19 10:59:14 +0900

    [fix] cors設定の本番向け修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
 	allow do
-		origins ENV["API_DOMAIN"] || ""
+		# origins ENV["API_DOMAIN"] || ""
+    origins ['https://dig-live.com', 'http://localhost:8080']
 
 		resource '*',
 		headers: :any,
