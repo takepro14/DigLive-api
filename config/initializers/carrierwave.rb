21db4d8f5817aacc6be326db34fcfee920077f3b
takepro14 <takepro100pm@gmail.com>
2022-10-21 23:53:09 +0900

    [feat] S3アップロードの実装

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,19 +6,20 @@ CarrierWave.configure do |config|
   # 本番環境 => S3へアップロード
   if Rails.env.production?
     config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'diglive-public-image' # バケット名
-    config.fog_public = false
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['SECRET_ACCESS_KEY_ID'],
-      region: 'ap-northeast-1',
-      path_style: true
+    config.fog_provider     = 'fog/aws'
+    config.fog_directory    = 'diglive-public-image' # バケット名
+    config.fog_public       = false
+    config.fog_credentials  = {
+      provider:               'AWS',
+      aws_access_key_id:      ENV['ACCESS_KEY_ID'],
+      aws_secret_access_key:  ENV['SECRET_ACCESS_KEY_ID'],
+      region:                 'ap-northeast-1',
+      path_style:             true
     }
   # 開発/テスト環境 => アプリケーション内にアップロード
   else
     config.storage :file
     config.enable_processing = false if Rails.env.test?
+    config.asset_host = ENV['IMAGE_URL']
   end
 end
