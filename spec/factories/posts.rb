5db144de3cc0b1fdd634191f572c575fd8b52888
takepro14 <takepro100pm@gmail.com>
2022-10-27 22:12:20 +0900

    [test] Postモデルのテストの追加

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :post do
     sequence(:content) { |n| "テスト投稿#{n}です。" }
+    youtube_url { "https://www.youtube.com/watch?v=0wF7P_UcET" }
     association :user
   end
 end
