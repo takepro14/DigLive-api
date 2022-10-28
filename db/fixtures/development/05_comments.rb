598ab427837a21243f77e5fbc0179779aeffcbac
takepro14 <takepro100pm@gmail.com>
2022-10-28 13:38:24 +0900

    [test] テストデータの修正

--- db/fixtures/development/05_comments.rb
+++ db/fixtures/development/05_comments.rb
@@ -5,12 +5,3 @@
     s.comment = Faker::Lorem.paragraph
   end
 end
-
-# 大量データ用
-100.times do
-  Comment.seed do |s|
-    s.user_id = rand(1..50)
-    s.post_id = rand(1..100)
-    s.comment = Faker::Lorem.paragraph
-  end
-end
--- db/fixtures/development/11_notificatios.rb
+++ db/fixtures/development/11_notificatios.rb
@@ -21,27 +21,3 @@
     s.comment_id = t + 1
   end
 end
-
-(10..50).each do |n|
-  # フォロー
-  Notification.seed do |s|
-    s.action = 'follow'
-    s.visitor_id = n + 1
-    s.visited_id = n + 2
-  end
-  # いいね
-  Notification.seed do |s|
-    s.action = 'like'
-    s.visitor_id = n + 1
-    s.visited_id = n + 2
-    s.post_id = n + 1
-  end
-  # コメント
-  Notification.seed do |s|
-    s.action = 'comment'
-    s.visitor_id = n + 1
-    s.visited_id = n + 2
-    s.post_id = n + 1
-    s.comment_id = n + 1
-  end
-end
