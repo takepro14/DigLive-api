c7f24bb12009d6b100f63489afcbeabc64c53037
takepro14 <takepro100pm@gmail.com>
2022-10-29 20:54:31 +0900

    [fix] アバター画像(デフォルト)表示されない不具合の対応

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -10,14 +10,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def default_url(*args)
-    if Rails.env.production?
-      "https://diglive-public-image.s3.amazonaws.com/uploads/#{model.class.to_s.underscore}/#{mounted_as}/default.png"
-    else
-      "#{ENV['IMAGE_URL']}/uploads/#{model.class.to_s.underscore}/#{mounted_as}/default.png"
-    end
-  end
-
   def extension_allowlist
     %w(jpg jpeg gif png)
   end
