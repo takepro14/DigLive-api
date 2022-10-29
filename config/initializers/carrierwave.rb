437dc4f6dfdd873b6977285653b63e3d5a3b1693
takepro14 <takepro100pm@gmail.com>
2022-10-29 16:03:09 +0900

    [fix] アバター画像(デフォルト)表示されない不具合の対応

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,10 +5,8 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
 
   if Rails.env.production?
-    config.storage :fog
-    config.fog_provider     = 'fog/aws'
+    config.cache_storage    = :fog
     config.fog_directory    = 'diglive-public-image'
-    config.fog_public       = false
     config.fog_credentials  = {
       provider:               'AWS',
       aws_access_key_id:      ENV['ACCESS_KEY_ID'],
@@ -18,6 +16,5 @@ CarrierWave.configure do |config|
   else
     config.asset_host         = ENV['IMAGE_URL']
     config.cache_storage      = :file
-    config.enable_processing  = false if Rails.env.test?
   end
 end
